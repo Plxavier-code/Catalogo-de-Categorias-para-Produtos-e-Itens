c9d5b637eb3d99cff2633fb573df066763f00a18
Pedro Lucas Dos Santos Xavier <pedrolucassxavier1@gmail.com>
2025-12-13 11:28:30 -0300

    implementa inserção de categoria e produto

--- arvore-catalogo-categorias/src/CatalogoControll.java
+++ arvore-catalogo-categorias/src/CatalogoControll.java
@@ -1,2 +1,17 @@
 public class CatalogoControll {
+    private Categoria raiz;
+    public void inserirCategoria(String nome, String CategoriaCodigo, String descricao){
+        Categoria novaCategoria= new Categoria(nome, CategoriaCodigo, descricao);
+        if (raiz== null){
+        raiz= novaCategoria;
+    }else{
+        raiz.adicionarFilho(novaCategoria);
+    }
+
+
+    }
+    public Produto inserirProduto(String nome, String produtoCodigo, double preco, String marca){
+        Produto novoProduto= new Produto(nome, produtoCodigo, preco, marca);
+        return novoProduto;
+    }
 }
